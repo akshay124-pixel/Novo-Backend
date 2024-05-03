fc789bf4b848b57080e5df5d18df1500f11b208e
akshay124-pixel <akshay124-pixel>
2024-05-03 16:52:31 +0530

    Final

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/api/admin", adminRoute);
 // Error Middleware
 app.use(errorMiddleware);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 connectDb().then(() => {
   app.listen(PORT, () => {
